e8f9262eeae88490aa6951085735c16dd626d97c
SvenEwald <github@ewaelder.germantld>
2012-12-23 23:17:48 +0100

    javadoc

--- src/test/java/org/xmlbeam/tutorial/e04_maven/TestMavenPOM.java
+++ src/test/java/org/xmlbeam/tutorial/e04_maven/TestMavenPOM.java
@@ -12,14 +12,18 @@ import org.xmlbeam.XMLProjector;
 
 public class TestMavenPOM {
 
+	/**
+	 * Show how to modify the project name in a maven POM.
+	 * 
+	 * @throws SAXException
+	 * @throws IOException
+	 * @throws ParserConfigurationException
+	 */
 	@Test
 	public void testProjectNameWriting() throws SAXException, IOException, ParserConfigurationException {
 		MavenPOM pom = new XMLProjector().readFromURIAnnotation(MavenPOM.class);
-		System.out.println(pom.toString());
 		assertEquals("Maven core", pom.getName());
 		pom.setName("New name");
-		System.out.println(pom.toString());
 		assertEquals("New name", pom.getName());
-
 	}
 }
