7fc329a4134641d1b6ec845f8f33fc0ae5bbd4fb
SvenEwald <github@ewaelder.germantld>
2012-12-23 23:06:12 +0100

    Javadoc

--- src/test/java/org/xmlbeam/tutorial/e07_svg/TestSVGElementSorting.java
+++ src/test/java/org/xmlbeam/tutorial/e07_svg/TestSVGElementSorting.java
@@ -9,6 +9,26 @@ import org.junit.Test;
 import org.xmlbeam.XMLProjector;
 import org.xmlbeam.tutorial.e07_svg.SVGDocument.GraphicElement;
 
+/**
+ * Demonstation of the concept of "projection mixins". While its not possible to
+ * let your own classes implements projection interfaces without losing the
+ * projection functionality, it is possible to define methods in a second
+ * interface called a mixin interface. If you let a projection extend a mixin
+ * interface, any object implementing the mixin interface can be registered to
+ * handle the method calls of the mixin interface. In this example the interface
+ * {@link Comparable} will be our mixin interface. Its possible to have a
+ * projection extending multiple mixin interfaces. Notice: Because the method
+ * call of a mixin method on a projection will not be executed by the projection
+ * instance itself but by your specified mixin implementation, we need a field
+ * to inject the current projection instance into. By convention this field must
+ * me named "me" (to resemble the keyword "this") and have the type of the
+ * projection. It may be private. <br>
+ * (Yes, the result of this example could have been archived by specifying a
+ * Comparator when calling Collections.sort().)
+ * 
+ * @author <a href="https://github.com/SvenEwald">Sven Ewald</a>
+ * 
+ */
 public class TestSVGElementSorting {
 
 	@Test
@@ -21,10 +41,8 @@ public class TestSVGElementSorting {
 
 			@Override
 			public int compareTo(GraphicElement o) {
-				assert me != null;
 				return me.getPosition().compareTo(o.getPosition());
 			}
-
 
 		});
 		List<GraphicElement> list = svgDocument.getGraphicElements();
