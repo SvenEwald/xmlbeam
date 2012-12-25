9032c1148714d240dad2978ba441925611758b2a
SvenEwald <github@ewaelder.germantld>
2012-12-25 08:28:48 +0100

    minor changes

--- src/test/java/org/xmlbeam/tutorial/e01_weather/WeatherData.java
+++ src/test/java/org/xmlbeam/tutorial/e01_weather/WeatherData.java
@@ -23,6 +23,15 @@ import org.xmlbeam.Xpath;
  * type conversion to primitives as integer and double and even a projection to
  * a structured sub-projection.
  * 
+ * The unit test will get some weather data like this:
+ * <weatherdata>
+ *     <weather alert="" degreetype="F" entityid="8172902" imagerelativeurl="http://blu.stc.s-msn.com/as/wea3/i/en-us/" isregion="False" lat="50.5520210266113" lon="6.24060010910034" provider="Foreca" region="" searchdistance="0" searchlocation="Monschau, Stadt Aachen, NW, Germany" searchresult="Monschau, North Rhine-Westphalia, Germany" searchscore="0.95" url="http://local.msn.com/worldweather.aspx?eid=8172902&amp;q=Monschau-DEU" weatherfullname="Monschau, North Rhine-Westphalia, Germany" weatherlocationcode="wc:8172902" weatherlocationname="Monschau, DEU" zipcode="">
+ *         <current skycode="31" skytext="Clear" temperature="46"/><
+ *     /weather>
+ * </weatherdata>
+ * 
+ * This interface will be used to access a selection of values from the xml data.
+ * 
  * @author <a href="https://github.com/SvenEwald">Sven Ewald</a>
  * 
  */
