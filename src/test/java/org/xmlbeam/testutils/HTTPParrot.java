ff63a9b3a1413118dba0f8c41a0d807e86270b24
SvenEwald <github.ewaelder.germantld>
2013-01-11 21:20:32 +0100

    minor changes

--- src/test/java/org/xmlbeam/testutils/HTTPParrot.java
+++ src/test/java/org/xmlbeam/testutils/HTTPParrot.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.xmlbeam.util;
+package org.xmlbeam.testutils;
 
 import java.io.IOException;
 import java.net.InetAddress;
