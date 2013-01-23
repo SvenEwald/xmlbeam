5d8b6155f75ec676dff2b580b23ba7ff93843a88
SvenEwald <github@ewaelder.germantld>
2013-01-23 23:02:11 +0100

    added mixins test

---
+++ src/test/java/org/xmlbeam/tests/mixins/TestMixins.java
@@ -0,0 +1,53 @@
+/**
+ *  Copyright 2013 Sven Ewald
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.xmlbeam.tests.mixins;
+
+import static junit.framework.Assert.assertNotNull;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Test;
+import org.xmlbeam.XBProjector;
+
+public class TestMixins {
+
+    public interface Mixin {
+        void doSomething();
+    }
+
+    public interface Projection extends Mixin {
+    }
+
+    final Mixin verifyMixin = mock(Mixin.class);
+    final Mixin mixin = new Mixin() {
+        private Projection me;
+
+        @Override
+        public void doSomething() {
+            assertNotNull(me);
+            verifyMixin.doSomething();
+        }
+
+    };
+
+    @Test
+    public void testMixinIsCalled() {
+        Projection projection = new XBProjector().mixins().addProjectionMixin(Projection.class, mixin).projectEmptyDocument(Projection.class);
+        projection.doSomething();
+        verify(verifyMixin).doSomething();
+    }
+}
