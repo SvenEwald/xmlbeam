489f8e6e6b8ea412e1c38da5fbbb0facff8625bd
SvenEwald <github.ewaelder.germantld>
2013-06-24 20:33:29 +0200

    Added some documentation source file

---
+++ src/test/java/org/xmlbeam/usage/SpecifyProjectorUsage.java
@@ -0,0 +1,36 @@
+/**
+ *  Copyright 2013 Sven Ewald
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.xmlbeam.usage;
+
+import java.io.IOException;
+
+import org.xmlbeam.XBProjector;
+import org.xmlbeam.XBProjector.Flags;
+
+/**
+ *
+ */
+public class SpecifyProjectorUsage {
+
+    public void testFluentAPI() throws IOException {
+
+        new XBProjector(Flags.SYNCHRONIZE_ON_DOCUMENTS).projectEmptyDocument(null);
+
+        new XBProjector(Flags.SYNCHRONIZE_ON_DOCUMENTS,Flags.TO_STRING_RENDERS_XML).io().fromURLAnnotation(null);
+
+    }
+
+}
