182058fbda06ca0878bb7e46d52c081ff021c1cb
SvenEwald <github@ewaelder.germantld>
2014-02-22 11:13:11 +0100

    minor fix in validation script

--- verify_signatures.sh
+++ verify_signatures.sh
@@ -2,5 +2,6 @@
 version=`xpath pom.xml "/project/version/text()"  2>/dev/null | tail -1`
 for type in -javadoc.jar -sources.jar .jar .pom ; do
 file="target/xmlprojector-$version$type"
-gpg --verify $file".asc" $file || exit 1 
+echo $file
+gpg --verify $file".asc" $file || exit 1 
 done
