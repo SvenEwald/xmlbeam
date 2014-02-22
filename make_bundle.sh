da7b41c46c6f18d2eb25c005719bf864ce73d405
SvenEwald <github@ewaelder.germantld>
2014-02-22 11:56:55 +0100

    Adding tiny shell skript for bundle creation. Maven repository plugin seems to be broken since 2010 :(

---
+++ make_bundle.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+version=`xpath pom.xml "/project/version/text()"  2>/dev/null | tail -1`
+cd target
+for type in -javadoc.jar -sources.jar .jar .pom ; do
+file="xmlprojector-$version$type"
+allfiles="$allfiles $file"
+done
+jar -cvf xmlbeam-$version"-bundle.jar" $allfiles
+cd ..
