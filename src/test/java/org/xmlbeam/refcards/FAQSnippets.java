1a34e1391c937e2132852024edb32dcffcdd0f23
SvenEwald <github@ewaelder.germantld>
2014-03-06 21:01:06 +0100

    Adding FAQ section about XPath implementation change

--- src/test/java/org/xmlbeam/refcards/FAQSnippets.java
+++ src/test/java/org/xmlbeam/refcards/FAQSnippets.java
@@ -15,11 +15,97 @@
  */
 package org.xmlbeam.refcards;
 
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
+import javax.xml.xpath.XPathFactoryConfigurationException;
+import javax.xml.xpath.XPathFunctionResolver;
+import javax.xml.xpath.XPathVariableResolver;
+
+import org.w3c.dom.Document;
 import org.xmlbeam.XBProjector;
+import org.xmlbeam.config.DefaultXMLFactoriesConfig;
+import org.xmlbeam.config.XMLFactoriesConfig;
 import org.xmlbeam.dom.DOMAccess;
 
 public class FAQSnippets {
     
+    /**
+     * @author sven
+     */
+    public class MyOwnXpathFactory extends XPathFactory {
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see javax.xml.xpath.XPathFactory#isObjectModelSupported(java.lang.String)
+         */
+        @Override
+        public boolean isObjectModelSupported(String objectModel) {
+            // TODO Auto-generated method stub
+            return false;
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see javax.xml.xpath.XPathFactory#setFeature(java.lang.String, boolean)
+         */
+        @Override
+        public void setFeature(String name, boolean value) throws XPathFactoryConfigurationException {
+            // TODO Auto-generated method stub
+
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see javax.xml.xpath.XPathFactory#getFeature(java.lang.String)
+         */
+        @Override
+        public boolean getFeature(String name) throws XPathFactoryConfigurationException {
+            // TODO Auto-generated method stub
+            return false;
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see
+         * javax.xml.xpath.XPathFactory#setXPathVariableResolver(javax.xml.xpath.XPathVariableResolver
+         * )
+         */
+        @Override
+        public void setXPathVariableResolver(XPathVariableResolver resolver) {
+            // TODO Auto-generated method stub
+
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see
+         * javax.xml.xpath.XPathFactory#setXPathFunctionResolver(javax.xml.xpath.XPathFunctionResolver
+         * )
+         */
+        @Override
+        public void setXPathFunctionResolver(XPathFunctionResolver resolver) {
+            // TODO Auto-generated method stub
+
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see javax.xml.xpath.XPathFactory#newXPath()
+         */
+        @Override
+        public XPath newXPath() {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+    }
+
     XBProjector projector = new XBProjector();
     
     {
@@ -50,6 +136,51 @@ public class FAQSnippets {
         };        
         projector.mixins().addProjectionMixin(Object.class, mixin);
         //END SNIPPET: MixinRegistration  
+    }
+
+    {
+        //START SNIPPET: OwnXPathFactoriesImplementation
+        XMLFactoriesConfig myConfig = new DefaultXMLFactoriesConfig() {
+            {
+                // We should change the behavior of the config to not
+                // interfere with the name space handling of an
+                // unknown XPath implementation.
+                setNamespacePhilosophy(NamespacePhilosophy.AGNOSTIC);
+            }
+
+            /**
+             * Override this method to inject your own factory.
+             */
+            @Override
+            public XPathFactory createXPathFactory() {
+                return new MyOwnXpathFactory();
+            }
+        };
+
+        XBProjector projector = new XBProjector(myConfig);
+        //END SNIPPET: OwnXPathFactoriesImplementation
+    }
+    {
+        //START SNIPPET: OwnXPathImplementation
+        XMLFactoriesConfig myConfig = new DefaultXMLFactoriesConfig() {
+            {
+                // We should change the behavior of the config to not
+                // interfere with the name space handling of an
+                // unknown XPath implementation.
+                setNamespacePhilosophy(NamespacePhilosophy.AGNOSTIC);
+            }
+
+            /**
+             * Or override this to bypass the factory and create your own XPath implementation here.
+             */
+            @Override
+            public XPath createXPath(Document... document) {
+                return super.createXPath(document);
+            }
+        };
+
+        XBProjector projector = new XBProjector(myConfig);
+        //END SNIPPET: OwnXPathImplementation
     }
 
 }
