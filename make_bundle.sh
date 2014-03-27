77c4b47ad0e97358a4ed145e07cb177699c8decd
SvenEwald <github@ewaelder.germantld>
2014-03-27 22:02:16 +0100

    minor fix in bundle creation script

--- make_bundle.sh
+++ make_bundle.sh
@@ -4,7 +4,7 @@ version=$1
 cd target
 for type in -javadoc.jar -sources.jar .jar .pom ; do
 file="xmlprojector-$version$type"
-allfiles="$allfiles $file"
+allfiles="$allfiles $file $file.asc"
 done
 jar -cvf xmlbeam-$version"-bundle.jar" $allfiles
 cd ..
