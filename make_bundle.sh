5c4f984dcadbbaa187310ef02375bcc460264efc
SvenEwald <github@ewaelder.germantld>
2014-03-27 21:27:04 +0100

    adaption for non MacOs hosted bundle building

--- make_bundle.sh
+++ make_bundle.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-version=`xpath pom.xml "/project/version/text()"  2>/dev/null | tail -1`
+#version=`xpath pom.xml "/project/version/text()"  2>/dev/null | tail -1`
+version=$1
 cd target
 for type in -javadoc.jar -sources.jar .jar .pom ; do
 file="xmlprojector-$version$type"
--- verify_signatures.sh
+++ verify_signatures.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-version=`xpath pom.xml "/project/version/text()"  2>/dev/null | tail -1`
+#version=`xpath pom.xml "/project/version/text()"  2>/dev/null | tail -1`
+version=$1
+echo checking version $version
 for type in -javadoc.jar -sources.jar .jar .pom ; do
 file="target/xmlprojector-$version$type"
 echo $file
