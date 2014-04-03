b31f1ca749a03c6c62b764609e94a818abf980d4
SvenEwald <github@ewaelder.germantld>
2014-04-03 21:13:01 +0200

    More unit test coverage,

--- src/test/java/org/xmlbeam/tests/types/TestStringBasedTypeConversion.java
+++ src/test/java/org/xmlbeam/tests/types/TestStringBasedTypeConversion.java
@@ -15,7 +15,9 @@
  */
 package org.xmlbeam.tests.types;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 
@@ -30,83 +32,94 @@ import org.xmlbeam.types.TypeConverter;
 public class TestStringBasedTypeConversion {
 
     private final TypeConverter typeConverter = new DefaultTypeConverter();
-    
+
     public static abstract class CustomClass {
         public final String param;
+
         protected CustomClass(final String param) {
-            this.param=param;
+            this.param = param;
         }
     }
-    
+
     public static class CustomObjectWithPrivateConstructor {
-       private CustomObjectWithPrivateConstructor(String string) {};
+        private CustomObjectWithPrivateConstructor(final String string) {
+        };
     }
 
     public static class CustomObjectThrowingException extends CustomClass {
-        public CustomObjectThrowingException(String s) throws IOException
-        { super(s);throw new IOException(); }
+        public CustomObjectThrowingException(final String s) throws IOException {
+            super(s);
+            throw new IOException();
+        }
     }
-    
+
     public static class CustomClassWithStringConstrucor extends CustomClass {
-      
-        public CustomClassWithStringConstrucor(String param) {
+
+        public CustomClassWithStringConstrucor(final String param) {
             super(param);
         }
     }
-    
+
     public static class CustomClassWithStaticFactory extends CustomClass {
-        private CustomClassWithStaticFactory(String param) {
+        private CustomClassWithStaticFactory(final String param) {
             super(param);
         }
-        public static CustomClassWithStaticFactory valueOf(String s) {
+
+        public static CustomClassWithStaticFactory valueOf(final String s) {
             return new CustomClassWithStaticFactory(s);
         }
     }
-    
+
     public static class CustomClassWithShortStaticFactory extends CustomClass {
-        private CustomClassWithShortStaticFactory(String param) {
+        private CustomClassWithShortStaticFactory(final String param) {
             super(param);
         }
-        public static CustomClassWithShortStaticFactory of(String s) {
+
+        public static CustomClassWithShortStaticFactory of(final String s) {
             return new CustomClassWithShortStaticFactory(s);
         }
     }
-    
+
     @Test
     public void ensureNotConvertable() {
-       assertFalse(typeConverter.isConvertable(Object.class));
-       assertFalse(typeConverter.isConvertable(CustomObjectWithPrivateConstructor.class));
+        assertFalse(typeConverter.isConvertable(Object.class));
+        assertFalse(typeConverter.isConvertable(CustomObjectWithPrivateConstructor.class));
     }
-    
-    @Test(expected=IOException.class)
+
+    @Test(expected = IOException.class)
     public void testConstructorThrowingException() throws Throwable {
         try {
-        ensureConversion(CustomObjectThrowingException.class);
+            ensureConversion(CustomObjectThrowingException.class);
         } catch (XBException e) {
             throw e.getCause();
         }
     }
-    
+
     @Test
     public void testStringBasedConstructor() {
         ensureConversion(CustomClassWithStringConstrucor.class);
     }
-    
+
     @Test
     public void testStaticFactoryBasedConversion() {
         ensureConversion(CustomClassWithStaticFactory.class);
     }
-    
+
     @Test
     public void testShortStaticFactoryBasedConversion() {
         ensureConversion(CustomClassWithShortStaticFactory.class);
     }
-    
-    private void ensureConversion(Class<? extends CustomClass> clazz) {
+
+    private void ensureConversion(final Class<? extends CustomClass> clazz) {
         assertTrue(typeConverter.isConvertable(clazz));
         CustomClass object = typeConverter.convertTo(clazz, "foo");
-        assertEquals("foo",object.param);
+        assertEquals("foo", object.param);
+
+        CustomClass object2 = typeConverter.convertTo(clazz, "");
+        assertEquals("", object2.param);
+
+        CustomClass object3 = typeConverter.convertTo(clazz, null);
+        assertEquals(null, object3.param);
     }
-    
-    
+
 }
