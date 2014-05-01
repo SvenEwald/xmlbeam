66906cca4301037e35d858764f174861d82174fb
SvenEwald <github@ewaelder.germantld>
2014-05-01 19:26:31 +0200

    Adding new annotation for future use. Declare a schema url for your projection interface and let the IDE validate your XPath.

---
+++ src/main/java/org/xmlbeam/annotation/XBSchemaURL.java
@@ -0,0 +1,42 @@
+/**
+ *  Copyright 2012 Sven Ewald
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.xmlbeam.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Declare a schema URL to a projection interface. Not used during runtime, but useful for 3rf party
+ * tools to validate projection interfaces.
+ *
+ * @author <a href="https://github.com/SvenEwald">Sven Ewald</a>
+ */
+@Retention(RetentionPolicy.SOURCE)
+@Target({ ElementType.METHOD, ElementType.TYPE })
+@Inherited
+public @interface XBSchemaURL {
+
+    /**
+     * A URL where the schema is located.
+     *
+     * @return value.
+     */
+    String value();
+
+}
