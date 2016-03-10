b5bcd12df21478d73408c2f9367d122e1956a38b
SvenEwald <github@ewaelder.germantld>
2016-03-10 23:21:01 +0100

    comments

---
+++ src/test/java/org/xmlbeam/tutorial/e17_svgTemplateInheritance/package-info.java
@@ -0,0 +1,20 @@
+/**
+ *    Copyright 2015 Sven Ewald
+ *
+ *    This file is part of JSONBeam.
+ *
+ *    JSONBeam is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, any
+ *    later version.
+ *
+ *    JSONBeam is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with JSONBeam.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.xmlbeam.tutorial.e17_svgTemplateInheritance;
+
