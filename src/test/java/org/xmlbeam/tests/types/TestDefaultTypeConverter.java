605d945e5a89a6ef20ae6d4a479cbfc1e4a53eac
SvenEwald <github@ewaelder.germantld>
2017-06-03 22:04:56 +0200

    more coverage

--- src/test/java/org/xmlbeam/tests/types/TestDefaultTypeConverter.java
+++ src/test/java/org/xmlbeam/tests/types/TestDefaultTypeConverter.java
@@ -18,10 +18,11 @@ package org.xmlbeam.tests.types;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import java.math.BigDecimal;
 import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
+
+import java.math.BigDecimal;
 
 import org.junit.Test;
 import org.xmlbeam.types.DefaultTypeConverter;
@@ -38,12 +39,12 @@ public class TestDefaultTypeConverter {
     @Test
     public void ensureConversionDefauls() {
         Object[] defaults = new Object[] { //
-        Boolean.class, null, Boolean.TYPE, Boolean.FALSE,//
-                Byte.class, null, Byte.TYPE, Byte.valueOf((byte) 0),//
-                Short.class, null, Short.TYPE, Short.valueOf((short) 0),//
-                Integer.class, null, Integer.TYPE, Integer.valueOf(0),//
-                Float.class, null, Float.TYPE, Float.valueOf(0),//
-                Double.class, null, Double.TYPE, Double.valueOf(0),//
+                Boolean.class, null, Boolean.TYPE, Boolean.FALSE, //
+                Byte.class, null, Byte.TYPE, Byte.valueOf((byte) 0), //
+                Short.class, null, Short.TYPE, Short.valueOf((short) 0), //
+                Integer.class, null, Integer.TYPE, Integer.valueOf(0), //
+                Float.class, null, Float.TYPE, Float.valueOf(0), //
+                Double.class, null, Double.TYPE, Double.valueOf(0), //
                 Long.class, null, Long.TYPE, Long.valueOf(0), //
                 Character.class, null, Character.TYPE, Character.valueOf(' ') //
         };
@@ -80,6 +81,8 @@ public class TestDefaultTypeConverter {
             assertEquals(Byte.valueOf((byte) -1), converter.convertTo(c, "-1"));
             assertEquals(Byte.valueOf(Byte.MAX_VALUE), converter.convertTo(c, "127"));
             assertEquals(Byte.valueOf(Byte.MIN_VALUE), converter.convertTo(c, "-128"));
+
+            assertEquals(Byte.valueOf((byte) 124), converter.convertTo(c, "$124", "$###"));
         }
     }
 
@@ -89,6 +92,52 @@ public class TestDefaultTypeConverter {
             assertEquals(Short.valueOf((short) -1), converter.convertTo(c, "-1"));
             assertEquals(Short.valueOf(Short.MIN_VALUE), converter.convertTo(c, "-32768"));
             assertEquals(Short.valueOf(Short.MAX_VALUE), converter.convertTo(c, "32767"));
+
+            assertEquals(Short.valueOf((short) 124), converter.convertTo(c, "$124", "$###"));
+        }
+    }
+
+    @Test
+    public void ensureFloat() {
+        for (Class<?> c : new Class<?>[] { Float.class, Float.TYPE }) {
+            assertEquals(Float.valueOf(-1f), converter.convertTo(c, "-1"));
+            assertEquals(Float.valueOf(-32768.0f), converter.convertTo(c, "-32768"));
+            assertEquals(Float.valueOf(32767.0f), converter.convertTo(c, "32767"));
+
+            assertEquals(Float.valueOf((short) 124), converter.convertTo(c, "$124", "$###"));
+        }
+    }
+
+    @Test
+    public void ensureDouble() {
+        for (Class<?> c : new Class<?>[] { Double.class, Double.TYPE }) {
+            assertEquals(Double.valueOf(-1d), converter.convertTo(c, "-1"));
+            assertEquals(Double.valueOf(-32768.0d), converter.convertTo(c, "-32768"));
+            assertEquals(Double.valueOf(32767.0d), converter.convertTo(c, "32767"));
+
+            assertEquals(Double.valueOf((short) 124), converter.convertTo(c, "$124", "$###"));
+        }
+    }
+
+    @Test
+    public void ensureInteger() {
+        for (Class<?> c : new Class<?>[] { Integer.class, Integer.TYPE }) {
+            assertEquals(Integer.valueOf(-1), converter.convertTo(c, "-1"));
+            assertEquals(Integer.valueOf(-32768), converter.convertTo(c, "-32768"));
+            assertEquals(Integer.valueOf(32767), converter.convertTo(c, "32767"));
+
+            assertEquals(Integer.valueOf(124), converter.convertTo(c, "$124", "$###"));
+        }
+    }
+
+    @Test
+    public void ensureLong() {
+        for (Class<?> c : new Class<?>[] { Long.class, Long.TYPE }) {
+            assertEquals(Long.valueOf(-1L), converter.convertTo(c, "-1"));
+            assertEquals(Long.valueOf(-32768L), converter.convertTo(c, "-32768"));
+            assertEquals(Long.valueOf(32767L), converter.convertTo(c, "32767"));
+
+            assertEquals(Long.valueOf(124L), converter.convertTo(c, "$124", "$###"));
         }
     }
 
@@ -104,7 +153,7 @@ public class TestDefaultTypeConverter {
     }
 
     @Test
-    public void ensureBigdecimal() {        
+    public void ensureBigdecimal() {
         assertEquals(new BigDecimal("123456"), new DefaultTypeConverter(Locale.US, TimeZone.getTimeZone("GMT")).convertTo(BigDecimal.class, "123,456", ""));
     }
 
