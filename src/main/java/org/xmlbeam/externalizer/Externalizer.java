55bc25fa043206cac4b75dc39d7b5cea3fdedc6d
SvenEwald <github@ewaelder.germantld>
2025-07-11 12:39:40 +0200

    Update license headers

--- src/main/java/org/xmlbeam/externalizer/Externalizer.java
+++ src/main/java/org/xmlbeam/externalizer/Externalizer.java
@@ -1,27 +1,4 @@
 /**
- *    Copyright 2015 Sven Ewald
- *
- *    This file is part of JSONBeam.
- *
- *    JSONBeam is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, any
- *    later version.
- *
- *    JSONBeam is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
- *
- *    You should have received a copy of the GNU General Public License
- *    along with JSONBeam.  If not, see <http://www.gnu.org/licenses/>.
- */
-package org.xmlbeam.externalizer;
-
-import java.lang.reflect.Method;
-
-import java.io.Serializable;
-/**
  *  Copyright 2013 Sven Ewald
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
@@ -36,6 +13,12 @@ import java.io.Serializable;
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+package org.xmlbeam.externalizer;
+
+import java.lang.reflect.Method;
+
+import java.io.Serializable;
+
 
 /**
  * This interface may be used to define a external source for projection metadata.
