5b90448b63830fe8fa3c17ef852a21c704d1b292
SvenEwald <github@ewaelder.germantld>
2025-07-11 12:31:33 +0200

    * Removed unintendet license headers * added Apache license header where missing

--- src/main/java/org/xmlbeam/util/intern/duplex/SimpleNode.java
+++ src/main/java/org/xmlbeam/util/intern/duplex/SimpleNode.java
@@ -1,17 +1,18 @@
+
 /*
-Copyright (c) 2005 W3C(r) (http://www.w3.org/) (MIT (http://www.lcs.mit.edu/),
-INRIA (http://www.inria.fr/), Keio (http://www.keio.ac.jp/)),
-All Rights Reserved.
-See http://www.w3.org/Consortium/Legal/ipr-notice-20000612#Copyright.
-W3C liability
-(http://www.w3.org/Consortium/Legal/ipr-notice-20000612#Legal_Disclaimer),
-trademark
-(http://www.w3.org/Consortium/Legal/ipr-notice-20000612#W3C_Trademarks),
-document use
-(http://www.w3.org/Consortium/Legal/copyright-documents-19990405),
-and software licensing rules
-(http://www.w3.org/Consortium/Legal/copyright-software-19980720)
-apply.
+ * Copyright 2025 Sven Ewald
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.xmlbeam.util.intern.duplex;
 
