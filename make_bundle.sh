0bc282921b6d49d2b5f8589f747c688305896f8a
Sven Ewald <SvenEwald@users.noreply.github.com>
2025-08-05 13:31:47 +0200

    Update make_bundle.sh
    Adopting to new sonatype checks

--- make_bundle.sh
+++ make_bundle.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
-#version=`xpath pom.xml "/project/version/text()"  2>/dev/null | tail -1`
 version=$1
+groupId="org/xmlbeam"
+artifactId="xmlprojector"
+
 cd target
+mkdir -p "$groupId/$artifactId/$version"
 for type in -javadoc.jar -sources.jar .jar .pom ; do
-file="xmlprojector-$version$type"
-allfiles="$allfiles $file $file.asc"
+    file="xmlprojector-$version$type"
+    cp "$file" "$groupId/$artifactId/$version/"
+    cp "$file.asc" "$groupId/$artifactId/$version/"
+    md5sum "$file" | cut -d' ' -f1 > "$groupId/$artifactId/$version/$file.md5"    
+    sha1sum "$file" | cut -d' ' -f1 > "$groupId/$artifactId/$version/$file.sha1"
 done
-jar -cvf xmlbeam-$version"-bundle.jar" $allfiles
+jar -cvMf xmlbeam-$version"-bundle.jar" "$groupId/"
 cd ..
